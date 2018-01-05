ffad663a2496bfa3c90cb2ffded26b3554269c52
Malte Hübner <maltehuebner@gmx.org>
2018-01-05 18:57:15 +0100

    Init.

---
+++ criticalmass.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Critical Mass
+Plugin URI: https://criticalmass.one/
+Description: Show critical mass related stuff in your blog
+Version: 0.1
+Author: Malte Hübner
+Author URI: https://maltehuebner.de
+License: MIT
+*/
