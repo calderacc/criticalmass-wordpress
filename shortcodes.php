967cd978c88fbfe2dd4ea2f85dc7c57bab0359ca
Malte Hübner <maltehuebner@gmx.org>
2018-01-17 03:29:21 +0100

    Somebody broke city list.

--- shortcodes.php
+++ shortcodes.php
@@ -38,19 +38,22 @@ function criticalmassRideList($attributeList = [], $content = null, $tag = '')
 
     foreach ($rideList as $ride) {
         $dateTime = new \DateTime(sprintf('@%d', $ride->dateTime));
+
         $dateTime->setTimezone($timezone);
 
         $cityLink = sprintf('https://criticalmass.in/%s', $ride->city->mainSlug->slug);
         $rideLink = sprintf('https://criticalmass.in/%s/%s', $ride->city->mainSlug->slug, $dateTime->format('Y-m-d'));
 
-        $o .= sprintf(
-            '<tr><td><a href="%s">%s</a></td><td><a href="%s">%s Uhr</a></td><td>%s</td><td>%s</td></tr>',
+        $row = sprintf(
+            '<tr><td><a href="%s">%s</a></td><td><a href="%s">%s Uhr</a></td><td>%s</td></tr>',
             $cityLink,
             $ride->city->name,
             $rideLink,
             $dateTime->format('d.m.Y H:i'),
             $ride->location
         );
+        
+        $o .= $row;
     }
 
     $o .= '</table>';
