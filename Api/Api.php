08f6c266edcaebaaeed2109e50884143273b7124
Malte Hübner <maltehuebner@gmx.org>
2018-01-17 23:23:04 +0100

    Add cache.

--- Api/Api.php
+++ Api/Api.php
@@ -1,15 +1,24 @@
 <?php
 
+require_once __DIR__ . '/../Cache/Cache.php';
+
 class Api
 {
+    /** @var Cache $cache */
+    protected $cache;
+
     public function __construct()
     {
-
+        $this->cache = new Cache();
     }
 
     public function fetch(string $endpoint, array $parameter = []): ?string
     {
         $apiUrl = sprintf('https://criticalmass.in/api/%s?%s', $endpoint, http_build_query($parameter));
+
+        if ($this->cache->isCached($apiUrl)) {
+            return $this->cache->get($apiUrl);
+        }
 
         $response = wp_remote_get($apiUrl);
 
@@ -18,6 +27,8 @@ class Api
         if (200 !== $responseCode) {
             return null;
         }
+
+        $this->cache->set($apiUrl, $response['body']);
 
         return $response['body'];
     }
---
+++ Cache/Cache.php
@@ -0,0 +1,25 @@
+<?php
+
+class Cache
+{
+    const TTL = 300;
+
+    public function __construct()
+    {
+    }
+
+    public function isCached(string $key): bool
+    {
+        return get_transient($key);
+    }
+
+    public function set(string $key, string $data): bool
+    {
+        return set_transient($key, $data, self::TTL);
+    }
+
+    public function get(string $key): ?string
+    {
+        return get_transient($key);
+    }
+}
