427bab041219780f871218272a0e8694204446dc
Malte Hübner <maltehuebner@gmx.org>
2018-01-21 19:47:02 +0100

    WIP.

--- Util/LinkUtil.php
+++ Util/LinkUtil.php
@@ -2,22 +2,24 @@
 
 class LinkUtil
 {
+    const HOSTNAME = 'https://criticalmass.in';
+
     public static function createLinkForRide(Ride $ride): string
     {
         $rideDate = $ride->getDateTime()->format('Y-m-d');
 
-        return sprintf('https://criticalmass.in/%s/%s', $ride->getCity()->getSlug(), $rideDate);
+        return sprintf('%s/%s/%s', self::HOSTNAME, $ride->getCity()->getSlug(), $rideDate);
     }
 
     public static function createLinkForRideEstimate(Ride $ride): string
     {
         $rideDate = $ride->getDateTime()->format('Y-m-d');
 
-        return sprintf('https://criticalmass.in/%s/%s/anonymousestimate', $ride->getcity()->getSlug(), $rideDate);
+        return sprintf('%s/%s/%s/anonymousestimate', self::HOSTNAME, $ride->getcity()->getSlug(), $rideDate);
     }
 
     public static function createLinkForCity(City $city): string
     {
-        return sprintf('https://criticalmass.in/%s', $city->getSlug());
+        return sprintf('%s/%s', self::HOSTNAME, $city->getSlug());
     }
 }
