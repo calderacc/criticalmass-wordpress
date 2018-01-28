8496cfc1cf00f29340ee58b4f4a7d29e7a9defb2
Malte Hübner <maltehuebner@gmx.org>
2018-01-28 18:33:29 +0100

    Add missing doppelpunkt.

--- Widget/CriticalmassWidget.php
+++ Widget/CriticalmassWidget.php
@@ -39,7 +39,7 @@ class CriticalmassWidget extends WP_Widget
 
         ?>
         <p>
-            <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Titel', 'caldera_criticalmass_widget'); ?></label>
+            <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Titel:', 'caldera_criticalmass_widget'); ?></label>
             <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
         </p>
 
